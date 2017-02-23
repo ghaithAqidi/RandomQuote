b29ab954a652c29c3709369213594eaeefb33220
Ghaith Ali <ghaithammar2008@gmail.com>
2017-02-24 00:16:56 +0100

    added json and api get method

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-/**
- * Created by GhaithAli on 23/02/2017.
- */
 $(document).ready(function(){
+    var elms = {
+        quoteText: $("#quoteText")
+    };
 
+    $("#quoteButton").on('click', function(event) {
+        //using this API to add data to our paragraph.
+        $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(json){
+            console.log(json.quoteText);
+            elms.quoteText.html(json.quoteText);
+        });
+    });
 });
