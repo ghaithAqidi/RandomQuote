5c600e7cd5f6942200b2dab3c0ccb3fdbb3282d8
Ghaith Ali <ghaithammar2008@gmail.com>
2017-03-18 10:21:20 +0100

    added twitter post api for quote and author. works.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
     var elms = {
-        quoteText: $("#quoteText")
+        quoteText: $("#quoteText"),
+        quoteAuthor: $("#quoteAuthor"),
+        tweetButton: $("#tweetButton")
     };
+
+    //the api website: http://forismatic.com/en/api/
 
     $("#quoteButton").on('click', function(event) {
         //using this API to add data to our paragraph.
         $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', function(json){
-            console.log(json.quoteText);
             elms.quoteText.html(json.quoteText);
+            elms.quoteAuthor.html(json.quoteAuthor);
+            //twit the quote.
+            var twitterText = "https://twitter.com/intent/tweet?text=" + json.quoteText + " Author: " + json.quoteAuthor;
+            elms.tweetButton.attr('href',twitterText);
+
         });
     });
 });
